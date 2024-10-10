eba9be30b08084d6578d6d91756d1965c599b157
joshua <joshua@localhost>
2024-10-09 20:01:26 -0700

    Still working on Lambda Methods

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,9 +13,17 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage(s->{
+			for (int i = s.length()-1; i >= 0; i--) {
+				System.out.print(s.charAt(i));
+			}
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage(s->{
+			for (int i = 0; i < s.length(); i++) {
+				
+			}
+		})
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
 		
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
